251e39a21e8c1f4b1a87c713e18ee164b44775f0
CCSClassBattlecruiser <623161603@qq.com>
2023-07-20 23:57:43 -0400

    角色数据目录

--- battle/data_parser.py
+++ battle/data_parser.py
@@ -1,7 +1,7 @@
 import openpyxl
 
 def paser_enemy_data():
-    path = 'enemy/enemy_base_data_by_666bj.xlsx'
+    path = 'battle_event/enemy/enemy_base_data_by_666bj.xlsx'
     wb_obj = openpyxl.load_workbook(path)
     sheet_obj = wb_obj.active
     print(type(sheet_obj))
---
+++ battle/enemy.py
@@ -0,0 +1,8 @@
+from battle.combat_obj import CombatObj
+
+
+class Enemy(CombatObj):
+
+    def __int__(self,event_list):
+
+        pass
