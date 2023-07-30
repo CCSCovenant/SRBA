a647345887969cdfe20088115b6281496d924e7f
CCSClassBattlecruiser <623161603@qq.com>
2023-07-29 23:08:05 -0400

    fix datamanager

--- battle/data_manager.py
+++ battle/data_manager.py
@@ -12,9 +12,12 @@ def singleton(cls):
 @singleton
 class DataManager(object):
     def __init__(self):
-        path = '../battle/battle_data/general_data/general_data_by_666bj_2.xlsx'
-        print(os.path.abspath(path))
-        self.character_data, self.light_cone_data, self.level_up_data, self.skill_stance_and_energy_recover, self.skill_damage_distributed, self.skill_damage_mul, self.character_path, = parser_general_data(path)
+        path = './battle_data/general_data/general_data_by_666bj_2.xlsx'
+        current_dir = os.path.abspath(__file__)
+        current_dir = os.path.dirname(current_dir)
+        data_path = os.path.join(current_dir,path)
+        data_path = os.path.normpath(data_path)
+        self.character_data, self.light_cone_data, self.level_up_data, self.skill_stance_and_energy_recover, self.skill_damage_distributed, self.skill_damage_mul, self.character_path, = parser_general_data(data_path)
 
 
 
