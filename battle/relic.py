fc705d4b94fafa26ed37c7d36d8c954343319405
CCSClassBattlecruiser <623161603@qq.com>
2023-07-29 23:49:40 -0400

    遗器

---
+++ battle/relic.py
@@ -0,0 +1,12 @@
+class Relic:
+    """
+    PART: 遗器的位置 0:头 1:手 2:躯干 3：脚部 4:球 5:绳
+    STAR: 遗器的星级
+    LEVEL: 遗器的等级
+    SUB_ATT: 遗器的副属性 是一个dict. key是副属性的id
+    SUB_ATT[dict] 储存对应的数值倍率(相对最小副属性基础值的倍率)
+
+    """
+    def __init__(self):
+
+        pass
